78a1ae8d674e50117fa62ed848fa92b9e2f7a40c
Chris Jarvis <clj@ollure.com>
2016-08-04 00:38:03 -0500

    tutorial completed

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 # NO LONGER NEEDED from django.http import HttpResponse
+from models import Treasure
 
 
 #Create views here
 def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context = {'treasure_name' : name, 'treasure_val': value}
-
-    return render(request, 'index.html', context)
+  #  name = 'Gold Nugget'
+   # value = 1000.00
+    #context = {'treasure_name' : name, 'treasure_val': value}
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasure': treasures})
 
