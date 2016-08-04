c0ae87192064f20a6e2bcb5b85bcf7c5d092c1e4
Chris Jarvis <clj@ollure.com>
2016-08-04 00:12:53 -0500

    refactored, added styling and templates,

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# NO LONGER NEEDED from django.http import HttpResponse
 
 
 #Create views here
 def index(request):
-    return HttpResponse('<h1>Hello Explorers!</h1>')
+    name = 'Gold Nugget'
+    value = 1000.00
+    context = {'treasure_name' : name, 'treasure_val': value}
+
+    return render(request, 'index.html', context)
+
